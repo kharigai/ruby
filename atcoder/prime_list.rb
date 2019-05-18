fe50c2fefc09d3ec79788335d789da055f48f11f
Kazuhiro Harigai <kharigai@gmail.com>
2019-05-19 08:16:08 +0900

    modified prime_list.rb

--- atcoder/prime_list.rb
+++ atcoder/prime_list.rb
@@ -1,18 +1,21 @@
 def get_prime_list(max)
-  prime_table = Array.new(max + 1){true}
-  prime_list = []
+  return [] if max < 0
+
+  # seve 篩（ふるい）
+  seve = Array.new(max + 1){true}
+  primes = []
 
   (2..max).each do |n|
-    if table[n] == true
-      prime << n
+    if seve[n] == true
+      primes << n
       k = n * n
       while k <= max
-        table[k] = false
+        seve[k] = false
         k += n
       end
     end
   end
-  table
+  primes
 end
 
 p prime_list(1000)
