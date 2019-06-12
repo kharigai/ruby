1d7ee7e5fb71e24dce22333c6b98dfa7ed1ab727
Kazuhiro Harigai <kharigai@gmail.com>
2019-06-12 13:08:43 +0900

    Bug fix

--- atcoder/broken_robot.rb
+++ atcoder/broken_robot.rb
@@ -1,25 +1,24 @@
-N = gets.strip.to_I
+N = gets.strip.to_i
 # EAST, WEST, SOUTH, NOTH
-PROBS = gets.strip.split.map(&:to_i)
+PROBS = gets.strip.split.map(&:to_f)
 
 @map = Array.new(100) { Array.new(100) { false } }
 
 @vx = [1, -1, 0,  0]
 @vy = [0,  0, 1, -1]
 
-@prob = 4.times.map { |i| PROBS[i] / 100 } 
-
+@prob = 4.times.map { |i| PROBS[i] / 100 }
 
 def dfd(x, y, n)
   return 0 if @map[y][x]
   return 1 if n.zero?
 
   @map[y][x] = true 
-  res = 0
+  res = 0.0
   4.times do |i|
     res += dfd(x + @vx[i], y + @vy[i], n - 1) * @prob[i] 
   end
-  @map[y][x] = true
+  @map[y][x] = false
 
   return res
 end
