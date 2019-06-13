466f38a00df98bd36b9239a35988cdc751de23b8
Kazuhiro Harigai <kharigai@gmail.com>
2019-06-13 21:07:08 +0900

    範囲指定が固定値だった問題を修正しました

--- leetcode/easy/reverse_integer.rb
+++ leetcode/easy/reverse_integer.rb
@@ -1,14 +1,19 @@
 # @param {Integer} x
 # @return {Integer}
+
+RANGE_MIN = -2 ** 31
+RANGE_MAX = 2 ** 31 - 1
+
 def reverse(x)
-  return 0 if x > 2147483647
   s = x.to_s.split('').reverse.join
   res = s.to_i
   unless s[-1].eql?('-')
-    res = 0 if res > 2147483647
+    res = 0 if res > RANGE_MAX
   else
     res *= -1
-    res = 0 if res < -2147483648
+    res = 0 if res < RANGE_MIN
   end    
   res
 end
+
+puts reverse(gets.strip.to_i)
