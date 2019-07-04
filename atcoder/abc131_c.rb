b88cbd494ddaa9d768a3559d04bd91e75361006a
Kazuhiro Harigai <kharigai@gmail.com>
2019-07-05 01:11:26 +0900

    add abc131 c

--- atcoder/abc131_c.rb
+++ atcoder/abc131_c.rb
@@ -1,21 +1,9 @@
 A, B, C, D = gets.strip.split.map(&:to_i) 
 
-max = 10 ** 9
+L = C.lcm(D)
 
-count = 0
-(1..max).each do |i|
-  c = C * i 
-  d = D * i
-  break if c > B && d > B
-  if c >= A && c <= B && (c % D) != 0
-    count += 1
-  end
-  if d >= A && d <= B && (d % C) != 0
-    count += 1
-  end
-  if d % C == 0
-    count += 1
-  end
+def num(x)
+  x - x / C - x / D + x / L
 end
 
-puts (B - A) + 1 - count
+puts num(B) - num(A - 1)
