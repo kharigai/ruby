2662e6bff316c39966a7bb5af1b79a0cbdc4925f
Kazuhiro Harigai <kharigai@gmail.com>
2019-07-08 00:27:04 +0900

    解説確認後、正解コードに修正しました。

--- atcoder/abc133_c.rb
+++ atcoder/abc133_c.rb
@@ -1,17 +1,18 @@
+def chmin(a, b) a < b ? a : b end
+
+def fin(x)
+  puts x
+  exit
+end
+
 L, R = gets.strip.split.map(&:to_i)
 
 res = 3000
-num = 0
-(L..R).each do |i| 
-  tmp = i % 2019
-  if tmp < res 
-    num = i
-    res = tmp
-  end
-  if res.zero?
-    puts 0
-    exit
+(L..R-1)).each do |i|
+  (i+1..R).each do |j|
+    res = chmin((i * j) % 2019, res)
+    fin(res) if res.zero?
   end
 end
 
-puts num.eql?(R) ? (res - 1) * res : res * (res + 1)
+puts res
