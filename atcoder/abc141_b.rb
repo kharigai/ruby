fe651e7e72e99cb8912f08a8c131aaddc0582776
Kazuhiro Harigai <kharigai@gmail.com>
2019-09-15 21:54:52 +0900

    add abc141 a, b, c

---
+++ atcoder/abc141_b.rb
@@ -0,0 +1,13 @@
+def fin(ok)
+  puts ok ? 'Yes' : 'No'
+  exit
+end
+
+gets.strip.chars.each_with_index do |c, i|
+  j = i + 1 
+  next if j.even? && ['L', 'U', 'D'].include?(c)
+  next if j.odd? && ['R', 'U', 'D'].include?(c)
+  fin false
+end
+fin true
+
