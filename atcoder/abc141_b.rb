212f075f617d60939358f57c5ad48dd4898a0a3d
Kazuhiro Harigai <kharigai@gmail.com>
2019-09-16 23:44:42 +0900

    exchange even and odd

--- atcoder/abc141_b.rb
+++ atcoder/abc141_b.rb
@@ -5,9 +5,8 @@ end
 
 gets.strip.chars.each_with_index do |c, i|
   j = i + 1 
-  next if j.even? && ['L', 'U', 'D'].include?(c)
   next if j.odd? && ['R', 'U', 'D'].include?(c)
+  next if j.even? && ['L', 'U', 'D'].include?(c)
   fin false
 end
 fin true
-
