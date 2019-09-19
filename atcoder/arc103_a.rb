0cdd5e268936331af62df04a97b6bd20c5884283
Kazuhiro Harigai <kharigai@gmail.com>
2019-09-19 14:05:32 +0900

    Refactoring

--- atcoder/arc103_a.rb
+++ atcoder/arc103_a.rb
@@ -1,13 +1,13 @@
-N = gets.strip.to_i
-s = gets.strip.split.map(&:to_i)
-
 hs = Array.new(2) { Hash.new(0) }
 hs[0][0] = 0
 hs[1][0] = 0
 # => [{0=>0}, {0=>0}]
 # この初期化によりハッシュ数（2）を保証する
 
-s.each_with_index { |n, i| hs[i%2][n] += 1 }
+N = gets.strip.to_i
+gets.strip.split.map(&:to_i).each_with_index do |n, i|
+  hs[i%2][n] += 1
+end
 
 e = hs[0].sort_by { |_, v| -v }
 o = hs[1].sort_by { |_, v| -v }
